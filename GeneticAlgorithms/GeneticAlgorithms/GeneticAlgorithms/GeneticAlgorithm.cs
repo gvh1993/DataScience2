3c5af203711e3399281183be07008d9c15fccadb
DESKTOP-7BMGD7J\gertj <gertjanvanh@live.nl>
2018-05-22 12:16:20 +0200

    added beginning genetic algorithms

---
+++ GeneticAlgorithms/GeneticAlgorithms/GeneticAlgorithms/GeneticAlgorithm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgorithms
+{
+    public class GeneticAlgorithm
+    {
+        public GeneticAlgorithm()
+        {
+            Individual ind = CreateIndividual();
+            double fittness = ComputeFitness(ind);
+        }
+        private Individual CreateIndividual()
+        {
+            Individual ind = new Individual();
+
+            Random r = new Random();
+            for (int i = 0; i < ind.Value.Length; i++)
+            {
+                double value = r.NextDouble();
+                if (value > 0.5)
+                {
+                    ind.Value[i] = true;
+                }
+                else
+                {
+                    ind.Value[i] = false;
+                }
+            }
+
+            return ind;
+        }
+
+        private double ComputeFitness(Individual ind)
+        {
+            int intValue = ind.CalculateFromByteArray();
+
+            //𝑓(𝑥) = −(𝑥^2)+7𝑥            double fitness = -(Math.Pow(intValue, 2)) + 7 * intValue;
+
+            return fitness;
+        }
+
+        private void SelectTwoParents(List<Individual> population)
+        {
+
+        }
+    }
+}
---
+++ GeneticAlgorithms/GeneticAlgorithms/GeneticAlgorithms/Individual.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgorithms
+{
+    public class Individual
+    {
+        public bool[] Value { get; set; }
+        public int Fitness { get; set; }
+
+
+        public Individual()
+        {
+            Value = new bool[5];
+        }
+
+        public int CalculateFromByteArray()
+        {
+            int totalValue = 0;
+            for (int i = Value.Length -1; i > 0; i--)
+            {
+                if (Value[i] == true)
+                {
+                    totalValue += (int)Math.Pow(i , 2);
+                }
+            }
+
+            return totalValue;
+        }
+    }
+}
